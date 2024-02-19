b703d3b9154def7cfd75e69d23818dd18f005e72
olabdulhaq <olabdulhaq@gmail.com>
2024-02-19 16:10:46 +0100

    content optimization

---
+++ src/utils/contants.js
@@ -0,0 +1,55 @@
+//HOME PAGE
+
+export const HERO_TEXT = {
+  primary: 'Achieve top 1% status with programs that ensures employability.',
+  secondary:
+    "Choose a course that suits you from leading universities, acquire valuable experience for today's job market through certifications equivalent to on-campus studies.",
+};
+
+//PROGRAMS
+export const PROGRAM_TITLE =
+  'Achieve certification and progress through our programs.';
+export const PROGRAM_DESC =
+  'We partner with the universities of your choice to make quality education accessible for everyone, from anywhere.';
+
+//EMPOWERMENT
+
+export const EMPOWERMENT_TITLE =
+  "Eduvacity's Efforts in Empowering African Youth for the Job Market";
+export const EMPOWERMENT_DESC =
+  'Bringing World-Class Education to Africa. Access Elite Resources Locally, Empowering Local Talent for Global Success.';
+
+export const EDUVACITY_VALUE =
+  'Immerse Yourself in Dynamic Learning Experiences tailored to Mirror Real-World Industry experience. Hone Your Expertise and Translate Theoretical Concepts into Practical Solutions in a Safe, Stimulating Environment.';
+
+//FOOTER
+export const FOOTER_NOTE =
+  'We bridge the education gap, helping individuals build technical, business, and interpersonal skills to excel in their careers or transition to new ones.';
+
+export const FOOTER_COURSES = [
+  'Computer Science',
+  'Computer Engineering ',
+  'Banking and Finance',
+  'Marketing and Communication',
+  'Business Administration',
+];
+
+export const FOOTER_COMPANY = [
+  { title: 'Why eduvacity', href: '/why-eduvacity' },
+  { title: 'Careers', href: '#' },
+  { title: 'Partners', href: '#' },
+  { title: 'Become an instructors', href: '#' },
+  { title: 'Scholarships', href: '#' },
+];
+
+//WHY EDUVACITY
+//
+export const WHY_EDUVACITY_TITLE =
+  'Embark on a journey into the future of disruptive learning';
+export const WHY_EDUVACITY_DESC =
+  'We are changing how you learn, grow, and succeed. Join us on a unique journey that leads innovation, accessibility, and excellence in your career pursuits.';
+
+export const WHY_EDUVACITY_VALUE =
+  'Access industry-relevant experiences tailored to your career goals, regardless of your location.';
+export const WHY_EDUVACITY_VALUE_DESC =
+  'Experience industry-specific opportunities tailored to your needs, regardless of your location, for a globally relevant skills and certification';
--- src/views/home/faq/index.js
+++ src/views/home/faq/index.js
@@ -1,113 +1,109 @@
-import { Box } from "@mui/material"
-import MuiAccordion from "@mui/material/Accordion"
-import MuiAccordionDetails from "@mui/material/AccordionDetails"
-import MuiAccordionSummary from "@mui/material/AccordionSummary"
-import { styled } from "@mui/material/styles"
-import React, { useState } from "react"
-import { MinusIcon, PlusIcon } from "../../../components/svg"
-import { Colors } from "../../../components/themes/colors"
-import { Fonts } from "../../../components/themes/fonts"
+import { Box } from '@mui/material';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import { styled } from '@mui/material/styles';
+import React, { useState } from 'react';
+import { MinusIcon, PlusIcon } from '../../../components/svg';
+import { Colors } from '../../../components/themes/colors';
+import { Fonts } from '../../../components/themes/fonts';
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(() => ({
   background: Colors.light,
-  borderBottom: "1px solid #EAECF0",
-  padding: "14px 0 0 0",
-  "&:not(:last-child)": {
+  borderBottom: '1px solid #EAECF0',
+  padding: '14px 0 0 0',
+  '&:not(:last-child)': {
     borderBottom: 0,
   },
-  "&:before": {
-    display: "none",
+  '&:before': {
+    display: 'none',
   },
-}))
+}));
 
 const AccordionSummary = styled((props) => <MuiAccordionSummary {...props} />)(
   () => ({
     backgroundColor: Colors.light,
-    textAlign: "left",
+    textAlign: 'left',
     font: `normal normal 400 18px/28px ${Fonts.secondary}`,
     letterSpacing: 0,
-    color: "#011B23",
+    color: '#011B23',
     padding: 0,
     minHeight: 10,
-    "& .MuiAccordionSummary-content": {
-      maxWidth: "100%",
+    '& .MuiAccordionSummary-content': {
+      maxWidth: '100%',
     },
   })
-)
+);
 
 const AccordionDetails = styled(MuiAccordionDetails)(() => ({
   padding: 0,
-  textAlign: "left",
+  textAlign: 'left',
   font: {
     xs: `normal normal 500 normal 16px/24px ${Fonts.primary}`,
     sm: `normal normal 500 normal 18px/24px ${Fonts.primary}`,
   },
   letterSpacing: 0,
-  color: "#868B93",
-}))
+  color: '#868B93',
+}));
 
 const questions = [
   {
-    question: "What types of courses does Eduvacity offer?",
+    question: 'What type of programs does eduvacity offers?',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      "Eduvacity offers a diverse range of higher education courses, including degree, diploma, master's, and doctoral programs, as well as bootcamps in various domains such as technology, finance, and business from renowned universities. This enables you to obtain quality certifications equivalent to those earned through on-campus studies.",
   },
   {
-    question: "How does the admission process work?",
+    question: 'How deos the admission process work?',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      'Apply for a program by submitting your application requirements, undergo review by the eduvacity team for admission eligibility, and upon approval, receive orientation and course enrollment details to kick off your classes.',
   },
   {
-    question:
-      "What technology-related courses are available through Eduvacity?",
+    question: 'What technology courses are available at eduvacity',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      'Apply for a degree by submitting your application requirements, undergo review by the Miva team for admission eligibility, and upon approval, receive orientation and course enrollment details to kick off your classes.',
   },
   {
-    question: "How are assessments and examinations conducted at eduvacity?",
+    question: 'How are assessments and examinations conducted at Eduvacity?',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      'Eduvacity conducts exams at its physical centers situated across various states in Nigeria, while For students located outside Nigeria, examinations are supervised remotely.',
   },
   {
-    question: "How do I apply for a program?",
+    question: 'Do I need to sit for the JAMB or UTME exams?',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      'Applying to Eduvacity do not require JAMB resultt, it  is optional at this time. However, upon university admission, the authenticity of your WAEC, NECO, GCE, or NABTEB results will be verified.',
   },
   {
-    question: "How much does tuition cost for Eduvacity programs?",
+    question: 'How much does tuition cost for eduvacity programs',
     answer:
-      "Eduvacity provides a diverse range of higher education courses, including degree, diploma, Masters and Doctorate programs, bootcamps in different domains, including, technology, finance, and business from renowned universities which allows you to earn quality certification as the one obtain in on campus studies. ",
+      'The starting point for our tuition fees is just 120,000 Naira per year. We also offer flexible payment plans to make financing your education convenient and manageable.',
   },
-]
+];
 export default function FrequentlyAskQuestions() {
-  const [expanded, setExpanded] = useState("panel1")
+  const [expanded, setExpanded] = useState('panel1');
 
   const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false)
-  }
+    setExpanded(newExpanded ? panel : false);
+  };
 
   return (
     <Box
       sx={{
-        width: "100%",
+        width: '100%',
         maxWidth: 768,
-      }}
-    >
+      }}>
       {questions?.map((quest, i) => (
-        <Box key={`quest-${i}`} sx={{ width: "100%" }}>
+        <Box key={`quest-${i}`} sx={{ width: '100%' }}>
           <Accordion
             expanded={expanded === `panel${i + 1}`}
-            onChange={handleChange(`panel${i + 1}`)}
-          >
+            onChange={handleChange(`panel${i + 1}`)}>
             <AccordionSummary
               aria-controls="panel1d-content"
               id="panel1d-header"
               expandIcon={
                 expanded === `panel${i + 1}` ? <MinusIcon /> : <PlusIcon />
-              }
-            >
+              }>
               {quest.question}
             </AccordionSummary>
             <AccordionDetails>{quest.answer}</AccordionDetails>
@@ -115,5 +111,5 @@ export default function FrequentlyAskQuestions() {
         </Box>
       ))}
     </Box>
-  )
+  );
 }
